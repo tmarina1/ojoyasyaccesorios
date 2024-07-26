0c571a499261140173316995240d583e3207a4dd
your username <tmarina@eafit.edu.co>
2024-07-26 14:54:54 -0500

    Avance en el registro de usurios

--- ojoyasyaccesorios/src/app/welcome/pages/register/register.component.ts
+++ ojoyasyaccesorios/src/app/welcome/pages/register/register.component.ts
@@ -1,10 +1,44 @@
+import { FormBuilder, Validators, FormControl } from '@angular/forms';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css']
+  styleUrls: ['./register.component.css'],
 })
 export class RegisterComponent {
+  constructor(private formBuilder: FormBuilder) {}
 
+  get name() {
+    return this.form_register_user.get('name') as FormControl;
+  }
+
+  get last_name() {
+    return this.form_register_user.get('last_name') as FormControl;
+  }
+
+  get phone() {
+    return this.form_register_user.get('phone') as FormControl;
+  }
+
+  get email() {
+    return this.form_register_user.get('email') as FormControl;
+  }
+
+  get address() {
+    return this.form_register_user.get('address') as FormControl;
+  }
+
+  get password() {
+    return this.form_register_user.get('password') as FormControl;
+  }
+
+  form_register_user = this.formBuilder.group({
+    name: ['', Validators.required],
+    last_name: ['', Validators.required],
+    phone: ['', Validators.required],
+    email: ['', [Validators.required, Validators.email]],
+    address: ['', Validators.required],
+    password: ['', [Validators.required]],
+  });
 }
--- ojoyasyaccesorios/src/app/welcome/welcome.module.ts
+++ ojoyasyaccesorios/src/app/welcome/welcome.module.ts
@@ -7,9 +7,20 @@ import { HomeComponent } from './pages/home/home.component';
 import { SharedModule } from '../shared/shared.module';
 import { LoginComponent } from './pages/login/login.component';
 import { RegisterComponent } from './pages/register/register.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [AboutUsComponent, HomeComponent, LoginComponent, RegisterComponent],
-  imports: [CommonModule, WelcomeRoutingModule, SharedModule],
+  declarations: [
+    AboutUsComponent,
+    HomeComponent,
+    LoginComponent,
+    RegisterComponent,
+  ],
+  imports: [
+    CommonModule,
+    WelcomeRoutingModule,
+    SharedModule,
+    ReactiveFormsModule,
+  ],
 })
 export class WelcomeModule {}
